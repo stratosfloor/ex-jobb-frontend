5df23d6c7dbfc25c3c17024d699bd92b5f76f57d
Emil Noren <stratosfloor@hotmail.com>
2023-01-20 14:56:24 +0100

    Add .env for google maps api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import styles from './App.module.css';
+import dotenv from 'dotenv';
+dotenv.config();
 
 function App() {
 	return <div className={styles.container}>main</div>;
