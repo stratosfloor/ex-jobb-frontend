f657990fee374bdda78910520b0f22fa9c9ba316
Emil Noren <stratosfloor@hotmail.com>
2023-01-24 15:33:38 +0100

    Add hook to add marker on map

--- src/Map.jsx
+++ src/Map.jsx
@@ -1,8 +1,30 @@
 import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 
 function Map({ center, zoom }) {
 	const ref = useRef(null);
 	const [map, setMap] = useState();
+	const [marker, setMarker] = useState({
+		lat: 57.682606003178826,
+		lng: 11.983962371493599,
+	});
+	const mossen = {
+		lat: 57.682606003178826,
+		lng: 11.983962371493599,
+	};
+
+	const addMarker = (location) => {
+		const marker = new google.maps.Marker({
+			position: location,
+			map: map,
+			// icon: {
+			//   path: google.maps.SymbolPath.CIRCLE,
+			//   scale: 10,
+			// }
+		});
+	};
+	addMarker(mossen);
+	useEffect(addMarker, []);
 
 	useEffect(() => {
 		if (ref.current && !map) {
@@ -11,6 +33,13 @@ function Map({ center, zoom }) {
 					center: center,
 					zoom: zoom,
 					mapTypeId: 'hybrid',
+					mapTypeControl: false,
+					rotateControl: false,
+					streetViewControl: false,
+					fullscreenControl: false,
+					panControl: false,
+					zoomControl: true,
+					draggableCursor: 'pointer',
 				})
 			);
 		}
