9b0ee891109f14aedd8a0e41f7ba19841d3f5ff5
Emil Noren <stratosfloor@hotmail.com>
2023-01-25 17:14:06 +0100

    Implemented ActiveReview component, show review when click on marker on map

--- src/ActiveReview.jsx
+++ src/ActiveReview.jsx
@@ -1,8 +1,19 @@
 import styles from './ActiveReview.module.css';
 
-const ActiveReview = ({ ActiveReview }) => {
-	// console.log(ActiveReview);
-	return <div className={styles.container}></div>;
+const ActiveReview = ({ activeReview }) => {
+	console.log(activeReview);
+	return (
+		<div className={styles.container}>
+			{' '}
+			{activeReview ? <h2>{activeReview[0].name}</h2> : null}
+			{activeReview
+				? activeReview.map((review, idx) => {
+						return <p key={idx}>{review.review}</p>;
+				  })
+				: null}
+			;
+		</div>
+	);
 };
 
 export default ActiveReview;
